071e36af4119253fddcdef2f6c47ed954a48c673
André Cassiano <andre.p.cassiano@accenture.com>
2022-06-23 07:58:49 -0300

    criando a classe position

---
+++ xadrez/src/application/App.java
@@ -0,0 +1,12 @@
+package xadrez.src.application;
+
+import xadrez.src.boardgame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
+
+
