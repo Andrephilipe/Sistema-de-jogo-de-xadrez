b263a3562e3c669e55b3200b580827fa0b46f54f
André Cassiano <andre.p.cassiano@accenture.com>
2022-07-17 10:44:35 -0300

    implementando os metodos move e mais alguns exceptions

--- xadrez/src/application/App.java
+++ xadrez/src/application/App.java
@@ -1,12 +1,29 @@
 package xadrez.src.application;
 
+import java.util.Scanner;
+
 import xadrez.src.chess.ChessMatch;
+import xadrez.src.chess.ChessPiece;
+import xadrez.src.chess.ChessPosition;
 
 public class App {
     public static void main(String[] args) throws Exception {
        
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
 
