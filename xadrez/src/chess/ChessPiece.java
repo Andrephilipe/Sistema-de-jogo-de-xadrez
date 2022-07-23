492c79ca45e74e94f74096eaa075e43e2a16f39d
André Cassiano <andre.p.cassiano@accenture.com>
2022-07-23 08:12:09 -0300

    Novas possobilidades de movimento das peças

--- xadrez/src/chess/ChessPiece.java
+++ xadrez/src/chess/ChessPiece.java
@@ -3,10 +3,10 @@ package xadrez.src.chess;
 import xadrez.src.boardgame.Board;
 import xadrez.src.boardgame.Piece;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
 
     private Color color;
-
+ 
     public Color getColor() {
         return color;
     }
